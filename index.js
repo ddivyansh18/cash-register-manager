d8bd6d7b25569a348b7dd56faa4e4a4100d192f2
Divyansh Dixit <ddivyansh18@gmail.com>
2021-08-24 19:54:40 +0530

    Completed basic design

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message")
+const availableNotes = [2000, 500, 100, 50, 10, 5, 1]
+const noOfNotes = document.querySelectorAll(".no-of-notes")
 
 console.log(cashGiven)
 
@@ -9,7 +11,7 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
     hideMessage();
     if(billAmount.value > 0) {
         if(cashGiven.value >= billAmount.value) {
-            const amountToBeReturned = cashGiven.value = billAmount.value;
+            const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
         } else {
             showMessage("Cash provided should be atleast equal to bill amount");
@@ -19,6 +21,14 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
     }
 
 })
+
+function calculateChange(amountToBeReturned) {
+    for(let i = 0; i < availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc(amountToBeReturned/availableNotes[i]); 
+        amountToBeReturned %= availableNotes[i]; 
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
 
 function hideMessage() {
     message.style.display = "none";
